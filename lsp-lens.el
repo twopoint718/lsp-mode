d4e78e2a5c2b036abce5c31e5b8367e203ad2f7a
Nikita Bloshchanevich <nikblos@outlook.com>
2020-11-03 22:09:29 +0100

    lsp-lens.el: fix byte-compile warnings

--- lsp-lens.el
+++ lsp-lens.el
@@ -145,16 +145,11 @@ BUFFER-MODIFIED? determines whether the buffer is modified or not."
 
 (defun lsp-lens--create-interactive-command (command?)
   "Create an interactive COMMAND? for the lens."
-  (let ((server-id (->> (lsp-workspaces)
-                        (cl-first)
-                        (or lsp--cur-workspace)
-                        (lsp--workspace-client)
-                        (lsp--client-server-id))))
-    (if (functionp (lsp:command-command command?))
-        (lsp:command-command command?)
-      (lambda ()
-        (interactive)
-        (lsp--execute-command command?)))))
+  (if (functionp (lsp:command-command command?))
+      (lsp:command-command command?)
+    (lambda ()
+      (interactive)
+      (lsp--execute-command command?))))
 
 (defun lsp-lens--display (lenses)
   "Show LENSES."
